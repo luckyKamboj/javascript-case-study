26fe631f869e1c43cec5c3c3ddd35e1550b1c2e6
lucky kamboj <luckykamboj125@gmail.com>
2026-09-02 22:18:03 +0530

    handle edge cases of counter application

--- projects/beginner/counterApp/counter.js
+++ projects/beginner/counterApp/counter.js
@@ -1,14 +1,29 @@
 document.addEventListener('click', function(e){
-   if(e.target.id === 'increment'){
+    if(e.target.id === 'increment'){
        const count = document.getElementById('count').innerHTML;
        document.getElementById('count').innerHTML = parseInt(count) + 1;
-   }
-   if(e.target.id === 'decrement'){ 
+    }
+    if(e.target.id === 'decrement'){ 
        const count = document.getElementById('count').innerHTML;
        document.getElementById('count').innerHTML = parseInt(count) - 1;
-   }
-   if(e.target.id === 'reset'){
+    }
+    if(e.target.id === 'reset'){
        const count = document.getElementById('count').innerHTML;
        document.getElementById('count').innerHTML = 0;
-   }
+    }
+        if(parseInt(document.getElementById('count').innerHTML) >= 10){
+        document.getElementById('increment').disabled = true;
+    }else{
+        document.getElementById('increment').disabled = false;
+    }
+    if(parseInt(document.getElementById('count').innerHTML) <= 0){
+        document.getElementById('decrement').disabled = true;
+    }else{
+        document.getElementById('decrement').disabled = false;
+    }
+    if(parseInt(document.getElementById('count').innerHTML) === 0){
+        document.getElementById('reset').disabled = true;
+    }else{
+        document.getElementById('reset').disabled = false;
+    }
 })
